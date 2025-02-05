fa4e756fa073ca9c7eefc065688e868d1bf3b1de
Attila Sinka <41182961+sinkaattila97@users.noreply.github.com>
2025-02-05 09:09:37 +0200

    Implemented custom tables for user login

--- spring-boot-spring-mvc-security/src/main/java/com/attilaslearning/demosecurity/security/DemoSecurityConfig.java
+++ spring-boot-spring-mvc-security/src/main/java/com/attilaslearning/demosecurity/security/DemoSecurityConfig.java
@@ -3,42 +3,43 @@ package com.attilaslearning.demosecurity.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+//import org.springframework.security.core.userdetails.User;
+//import org.springframework.security.core.userdetails.UserDetails;
+//import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class DemoSecurityConfig {
 
+    //add support for JDBC. No more hardcoded users
+    //Added custom tables to not user the default users/ authorities tables
     @Bean
-    public InMemoryUserDetailsManager userDetailsManager() {
-        UserDetails john = User.builder()
-                .username("john")
-                .password("{noop}test123")
-                .roles("EMPLOYEE")
-                .build();
+    public UserDetailsManager userDetailsManager(DataSource dataSource) {
+        JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
 
-        UserDetails mary = User.builder()
-                .username("mary")
-                .password("{noop}test123")
-                .roles("EMPLOYEE", "MANAGER")
-                .build();
+        //define queries to retrieve user by username
+        jdbcUserDetailsManager.setUsersByUsernameQuery(
+                "select user_id, pw, active from members where user_id=?");
 
-        UserDetails susan = User.builder()
-                .username("susan")
-                .password("{noop}test123")
-                .roles("EMPLOYEE", "MANAGER", "ADMIN")
-                .build();
+        //define queries to retrieve the authorities/roles by username
+        jdbcUserDetailsManager.setAuthoritiesByUsernameQuery(
+                "select user_id, role from roles where user_id=?");
 
-        return new InMemoryUserDetailsManager(john, mary, susan);
-
+        return jdbcUserDetailsManager;
     }
+
+
+
 
     /*
     * Authorizes HTTP Requests: It ensures that any request to the application is authenticated.
     *Configures Form Login: It sets up a custom login page (/showMyLoginPage) and specifies the URL (/authenticateTheUser) to process the login form submissions.
     * It also allows unrestricted access to the login page */
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
         http.authorizeHttpRequests(configurer ->
@@ -62,4 +63,28 @@ public class DemoSecurityConfig {
                 );
         return http.build();
     }
+
+    /*@Bean
+    public InMemoryUserDetailsManager userDetailsManager() {
+        UserDetails john = User.builder()
+                .username("john")
+                .password("{noop}test123")
+                .roles("EMPLOYEE")
+                .build();
+
+        UserDetails mary = User.builder()
+                .username("mary")
+                .password("{noop}test123")
+                .roles("EMPLOYEE", "MANAGER")
+                .build();
+
+        UserDetails susan = User.builder()
+                .username("susan")
+                .password("{noop}test123")
+                .roles("EMPLOYEE", "MANAGER", "ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(john, mary, susan);
+
+    }*/
 }
